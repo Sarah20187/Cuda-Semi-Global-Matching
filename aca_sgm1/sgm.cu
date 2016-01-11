0303d8a187c8a5c4ca6ae656946d1e4964098b9a
andre.lopes@ua.pt <andre.lopes@ua.pt>
2016-01-11 15:11:08 +0000

    fix costs fill ex1

--- aca_sgm1/sgm.cu
+++ aca_sgm1/sgm.cu
@@ -369,12 +369,15 @@ __global__ void determine_costs_k(const int *left_image, const int *right_image,
 #define DISP_IMAGE(i,j)           disp_image[(i)+(j)*nx]*/
 
 
-
+std::fill(costs, costs+nx*ny*disp_range, 255u);
   //we need to replace this for with something
   for ( int d = 0; d < disp_range; d++ ) {
     // Macros inside kernel? What do we need to change?
    // COSTS(i,j,d) = abs( LEFT_IMAGE(i,j) - RIGHT_IMAGE(i-d,j) );
    //no macro alternative (safer?)
+   if(costs[(i)*disp_range+(j)*nx*disp_range+(d)] < 0)
+      costs[(i)*disp_range+(j)*nx*disp_range+(d)] = 255u; 
+  else
       costs[(i)*disp_range+(j)*nx*disp_range+(d)] = abs( left_image[(i) + (j)*nx] - right_image[(i-d)+(j)*nx] );
   //  COSTS(i,j,d) = abs( LEFT_IMAGE(i,j) - RIGHT_IMAGE(i-d,j) );
   }
@@ -461,7 +464,7 @@ int imageSize = nx * ny * sizeof(int);  //image size in bytes
   dim3 block(block_x,block_y);
   dim3 grid(grid_x, grid_y);
 
-  std::fill(costs, costs+nx*ny*disp_range, 255u);
+
 
   determine_costs_k <<< grid, block >>> (left_image,right_image,costs,disp_range,nx,ny);
 
