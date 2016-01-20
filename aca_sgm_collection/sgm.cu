97df37c614d2a8ab57dba4fe74ae08d120ed8046
mglcampos <mgl.mcampos@gmail.com>
2016-01-20 17:39:45 +0000

    changes3 in collection

--- aca_sgm_collection/sgm.cu
+++ aca_sgm_collection/sgm.cu
@@ -627,7 +627,7 @@ void sgmDevice( const int *h_leftIm, const int *h_rightIm,
   int imageSize = nx * ny * sizeof(int);  //image size in bytes
   int size = nx * ny * disp_range * sizeof(int);
 
-  int *left_image, *right_image, *dev_costs,accumulated_costs,ddir_accumulated_costs;
+  int *left_image, *right_image, *dev_costs,*daccumulated_costs,*ddir_accumulated_costs;
 
 
   int *costs = (int *) calloc(nx*ny*disp_range,sizeof(int));
